e7f6026ba231fe2052b9581f166384a3a81a390c
Cameron Chin <31452808+cmc6887@users.noreply.github.com>
2019-11-20 22:10:10 -0500

    Big CSS Changes
    Redid whole css for site and nav bar

--- index.php
+++ index.php
@@ -7,16 +7,19 @@
 </head>
 
 <body>
-    <header>
-        <a class="logo" href="./"><img src="assets/images/Logo.png"></a>
-        <?php include "linuxnav.html"; ?>
-    </header>
-
+    <div class="banner">
+         <header>
+             
+             <a class="logo" href="./"><img src="assets/images/Logo.png"></a>
+             <?php include "linuxnav.html"; ?>
+             
+             
+             <h1 class= "glow" class="banner-text">About</h1>
+        
+         </header>
+    </div>
 
     <div class="content">
-        <div class="banner">
-            <h1 class="banner-text">About</h1>
-        </div>
 
         <!-- load in the content via json -->
         <?php
