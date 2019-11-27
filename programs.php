275214cca3926aff808dbe0e6d2b7c2c5c02ce4f
Estrella <estrella.carcamo@outlook.com>
2019-11-27 15:40:07 -0500

    Update programs.php

--- programs.php
+++ programs.php
@@ -1,35 +1,26 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+		<title>Programs</title>
+	</head>
+	<body>
+		<header class="banner">
+			<a class="logo" href="./"><img src="assets/images/Logo.png"></a>
+			<?php include "linuxnav.html"; ?>
+			
+			<h1 class="glow" class="banner-text">Programs</h1>
+		</header>
 
-<head>
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-    <title>Programs</title>
-</head>
+		<div class="content">
+			
+			<?php 
+			$jsonfile= "assets/json/programs.json";
+			include "loadjson.php:"
+			?>
+			
+		</div>
 
-<body>
-    <div class="banner">
-         <header>
-             
-             <a class="logo" href="./"><img src="assets/images/Logo.png"></a>
-             <?php include "linuxnav.html"; ?>
-             
-             
-             <h1 class= "glow" class="banner-text">Linux Events</h1>
-        
-         </header>
-    </div>
 
-    <div class="content">
-
-        <?php
-        
-            echo "<p>Here are some events revolving around Linux that are coming up around the world!";
-        
-        ?>
-        <div class="youtube">
-            <iframe src="https://calendar.google.com/calendar/b/1/embed?height=300&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=Zy5yaXQuZWR1X2gwbDQydWMwN3BoNzZlbGtmNTV0bnNkNXA0QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%23515151&amp;showNav=0&amp;showCalendars=1&amp;showPrint=0&amp;showTz=0&amp;showDate=1&amp;showTabs=1" style="border-width:0" width="400" height="300" frameborder="0" scrolling="no"></iframe>
-        </div>
-    </div>
-</body>
-
+	</body>
 </html>
